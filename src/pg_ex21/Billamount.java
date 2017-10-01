e6951a4e5a7649aee48cedece09c9d3f1870aa53
takeda <takeda@user-PC>
2017-10-02 03:20:26 +0900

    計算部分を追記

--- src/pg_ex21/Billamount.java
+++ src/pg_ex21/Billamount.java
@@ -17,6 +17,9 @@ public class Billamount {
 		ArrayList<String> recordlinejudgeTest = new ArrayList<String>();
 		 billamount.recordlinejudge(recordlinejudgeTest);
 
+		 CustomerBean customerBean = new CustomerBean();
+		 billamount.billmath(customerBean);
+
 //		ArrayList<String> recordjudgeData = new ArrayList<String>();
 //		 billamount.recordlinedivide(recordjudgeData);
 
@@ -53,18 +56,70 @@ public class Billamount {
 					customerBean.setPhoneNumber(recordlinejudgeTest.get(i).substring(2));
 					System.out.println(customerBean.getPhoneNumber());
 				}else if(frirst == 2){
-				customerBean.setValueCampaignInfoList(recordlinejudgeTest.get(i).substring(2));
-				System.out.println(customerBean.getValueCampaignInfoList(0));
+					customerBean.setValueCampaignInfoList(recordlinejudgeTest.get(i).substring(2));
+					System.out.println(customerBean.getValueCampaignInfoList(0));
 				}else if(frirst == 5){
-				customerBean.setValueCallInfoList(recordlinejudgeTest.get(i).substring(2));
-				System.out.println(customerBean.getValueCallInfoList(0));
+					customerBean.setValueCallInfoList(recordlinejudgeTest.get(i).substring(2));
+					System.out.println(customerBean.getValueCallInfoList(0));
 			}
 		}
 		return customerBean;
 	}
 
+	//顧客ごとの請求額計算メソッド
+	public ArrayList<String> billmath(CustomerBean customerBeanbill) {
+		ArrayList<String> billresult = new ArrayList<String>();
+		int basicCharge = 1000;
+		int callCharge = 1000;
+		int callpriceAmount = 0;
+		ArrayList<String> familydiscountnumber = new ArrayList<String>();
 
+		//加入サービスの特定と基本料金の計算
+		for (int i = 0; i < customerBeanbill.getCampaignInfoList().size(); i++){
+			String servicetype = customerBeanbill.getValueCampaignInfoList(i).substring(0,2);
+			if(servicetype.equals("C1")){
+				basicCharge = 1100;
+				familydiscountnumber.add(customerBeanbill.getValueCampaignInfoList(i).substring(3,16));
+			}else if(servicetype == "E1"){
+				basicCharge +=100;
+			}
+		}
 
+		//通話記録の特定と通話料金の計算
+		for (int i = 0; i < customerBeanbill.getCallInfoList().size(); i++){
+			int callUnitPrice = 20;
+			int time = Integer.parseInt(customerBeanbill.getValueCallInfoList(i).substring(11,13).replace("0",""));
+			System.out.println(time);
+			int callMinute = Integer.parseInt(customerBeanbill.getValueCallInfoList(i).substring(17,20).replace("0",""));
+			System.out.println("分"+callMinute);
+			String phonenum = customerBeanbill.getValueCallInfoList(i).substring(21,34);
+			System.out.println(phonenum);
+
+			//割引計算
+			if(time>=8 || time<=17) {
+				callUnitPrice -= 5;
+				for (String numcheck : familydiscountnumber){
+					if(numcheck.equals(phonenum)){
+						callUnitPrice /= 2;
+					}
+				}
+			}else{
+				for (String numcheck : familydiscountnumber){
+					if(numcheck.equals(phonenum)){
+						callUnitPrice /= 2;
+					}
+				}
+			}
+			callpriceAmount  += callUnitPrice * callMinute;
+			System.out.println("料金は"+ callpriceAmount);
+		}
+
+		billresult.add("1 " + customerBeanbill.getPhoneNumber());
+		billresult.add("5 " + String.valueOf(basicCharge));
+		billresult.add("7 " + String.valueOf(callpriceAmount));
+		billresult.add("9 ====================");
+		return billresult;
+	}
 
 
 	//スペースごとに分轄してにwrodList配列に格納するメソッド
--- src/pg_ex21/BillamountTest.java
+++ src/pg_ex21/BillamountTest.java
@@ -1,13 +1,12 @@
 package pg_ex21;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JTextArea;
 
 import org.junit.Test;
+
 
 
 public class BillamountTest {
@@ -26,17 +25,36 @@ public class BillamountTest {
 //		}
 	}
 
+//	@Test
+//	public void recordlinejudgeTest() {
+//		List<String> receiveRecordreturn = new ArrayList<String>();
+//		JTextArea area = new JTextArea("1 090-1234-0001\n2 C1 090-1234-0002\n2 C1 090-1234-0003\n5 2004/06/04 03:34 003 090-1234-0002\n5 2004/06/04 13:50 010 090-1234-9999");
+//		receiveRecordreturn = billamount.receiveRecord(area);
+//		assertEquals("[1, 090-1234-0001\n2, C1, 090-1234-0002]", billamount.recordlinejudge(receiveRecordreturn));
+//	}
+
 	@Test
-	public void recordlinejudgeTest() {
-		List<String> recordlinejudgeTest = new ArrayList<String>();
+	public void billmathTest(){
+		List<String> receiveRecordReturn = new ArrayList<String>();
+		CustomerBean recordlinejudgeReturn = new CustomerBean();
 		JTextArea area = new JTextArea("1 090-1234-0001\n2 C1 090-1234-0002\n2 C1 090-1234-0003\n5 2004/06/04 03:34 003 090-1234-0002\n5 2004/06/04 13:50 010 090-1234-9999");
-		recordlinejudgeTest = billamount.receiveRecord(area);
-		assertEquals("[1, 090-1234-0001\n2, C1, 090-1234-0002]", billamount.recordlinejudge(recordlinejudgeTest));
+		receiveRecordReturn = billamount.receiveRecord(area);
+		recordlinejudgeReturn = billamount.recordlinejudge(receiveRecordReturn);
+
+		String st = null;
+		ArrayList<String> billresultTest = new ArrayList<String>();
+		billresultTest = billamount.billmath(recordlinejudgeReturn);
+
+			assertEquels("1 090-1234-0001\n",billresultTest.get(0));
+			assertEquels("5 1100\n",billresultTest.get(0));
+			assertEquels("7 230\n",billresultTest.get(0));
+			assertEquels("9 ====================",billresultTest.get(0));
+
 	}
 
-	@Test
-	public void billmath()
+private void assertEquels(String string, String string2) {
+	// TODO 自動生成されたメソッド・スタブ
 
-	}
+}
 
 }
